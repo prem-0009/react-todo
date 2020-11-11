5d7632381898e5ea2c118f94b4c38e5f7f067484
prem <prem.sherpa@codeimmersives.com>
2020-11-11 18:16:57 -0500

    see todo-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Todo from './components/Todo.js'
+import TodoView from './components/TodoView.js';
+
+
+
+class App extends Component {
+  
+  render() {
+    
+    return (
+      <div>
+        
+
+      <TodoView/>
+      <Todo/>
+
+
+        
+      </div>
+    )
+  }
 }
+
 
 export default App;
